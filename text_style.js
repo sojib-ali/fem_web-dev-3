1e4752e8cc7b369bfdb494ad50900981a42f9187
Sojib Ali <sojib.ali@hotmail.com>
2023-10-14 14:38:34 +0600

    completed colablane landing page

---
+++ text_style.js
@@ -0,0 +1,46 @@
+const text = "Two shadows merge, weaving a tale yet to be told"; // Your text here
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("typewriter").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, 100); // Adjust the typing speed here
+  } else {
+    i = 0;
+    document.getElementById("typewriter").innerHTML = '';
+    setTimeout(typeWriter, 100); // Adjust the time before the text restarts typing
+  }
+}
+typeWriter();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const typewriter = document.getElementById('typewriter');
+// const text = 'Two shadows merge, weaving a tale yet to be told';
+// let index = 0;
+// function type() {
+//   if (index < text.length) {
+//     typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">|</span>';
+//     index++;
+//     setTimeout(type, Math.random() * 150 + 50);
+//     } else {
+//       typewriter.innerHTML = text.slice(0, index) + '<span class="blinking-cursor">|</span>';
+//     }
+// }
+// // start typing
+
+//   type();
+
